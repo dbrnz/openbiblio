cd16b2e03d05c37c90f621b45c0d9eaa22d14da8
Luuk Jansen <luck_luuk@msn.com>
2009-12-17 11:17:38 +0000

    Advanced search implemented except AudienceLevel (next to do)

---
+++ opac/index.php
@@ -0,0 +1,27 @@
+<?php
+/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+ * See the file COPYRIGHT.html for more details.
+ */
+
+##### temporary redirection to force move to 'Local Search'
+header("Location: ../catalog/biblio_search.php?tab=OPAC");
+
+require_once("../shared/common.php");
+
+session_cache_limiter(null);
+
+$tab = "opac";
+$nav = "home";
+$focus_form_name = "catalog_search";
+$focus_form_field = "searchText";
+
+Page::header_opac(array('nav'=>$nav, 'title'=>''));
+
+$lookup = "N";
+if (isset($_GET["lookup"])) {
+	$lookup = "Y";
+}
+
+include(REL(__FILE__, "../shared/searchbox.php"));
+
+Page::footer();
